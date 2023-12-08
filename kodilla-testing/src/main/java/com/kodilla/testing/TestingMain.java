92bb317d1081b84caa32345ad14abe560b8b1cc3
Adrian Tretyn <adriantretyn@gmail.com>
2023-12-08 09:11:27 +0100

    Task 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania test ");
+//        SimpleUser simpleUser = new SimpleUser("theForumUser");
+//        String result = simpleUser.getUsername();
+//        if (result.equals("theForumUser")) {
+//            System.out.println("test OK");
+//        } else {
+//            System.out.println("Error!");
+//        }
+
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.addnumbers(20 , 10);
+        int subResult = calculator.subtract(20 , 12);
+
+        if (addResult == 30 && subResult == 10){
+            System.out.println("Test OK");
+        }
+        else {
+            System.out.println("Error");
+        }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+
+    public int addnumbers(int a , int b){
+        return a + b ;
+
+    }
+    public int subtract (int a , int b ){
+        return a- b ;
+    }
+}
