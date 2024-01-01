8c3c16dcc742e23a029e53fb4755cd316b890423
Adrian Tretyn <adriantretyn@gmail.com>
2024-01-01 13:42:55 +0100

    improvement of additional tasks added exception 0

--- kodilla-exception/src/main/java/com/kodilla/exception/io/WeAreAPower.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/WeAreAPower.java
@@ -4,8 +4,9 @@ public class WeAreAPower {
 
     public static int calculatePower(int base , int exponet){
         int result = 1 ;
-
-
+        if (base == 0) {
+            return result = 1;
+        }
         for ( int i = 0 ; i < exponet ; i ++){
             result = result * base;
         }
@@ -15,12 +16,14 @@ public class WeAreAPower {
 
     public static void main(String[] args){
 
-        int ressult2 = calculatePower(1 , 0);
-        System.out.println(ressult2);
-        int ressult1 = calculatePower(4 , 2);
+        int ressult1 = calculatePower(1 , 0);
         System.out.println(ressult1);
-        int ressult3= calculatePower(8 , 2);
+        int ressult2 = calculatePower(2 , 2);
+        System.out.println(ressult2);
+        int ressult3 = calculatePower(4 , 2);
         System.out.println(ressult3);
+        int ressult4= calculatePower(8 , 2);
+        System.out.println(ressult4);
     }
 
 
