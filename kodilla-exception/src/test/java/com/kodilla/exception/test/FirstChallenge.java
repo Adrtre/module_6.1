1c2059a02101fb479674c2124be6da984ab274e2
Adrian Tretyn <adriantretyn@gmail.com>
2024-01-02 13:36:25 +0100

    add Test

--- kodilla-exception/src/test/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/FirstChallenge.java
@@ -1,2 +1,30 @@
-package com.kodilla.exception.test;public class FirstChallenge {
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            FirstChallenge firstChallenge = new FirstChallenge();
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        }
+        catch(ArithmeticException e ){
+            System.out.println("Coś nie bangla  "+ e );
+        }
+        finally {
+            System.out.println("Zakończenie");
+        }
+
+    }
 }
