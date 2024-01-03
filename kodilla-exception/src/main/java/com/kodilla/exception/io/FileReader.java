e759102ac809d47c7a9542b08e2969edcaea364d
Adrian Tretyn <adriantretyn@gmail.com>
2024-01-03 13:39:37 +0100

    Separate RPS on class

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -1,19 +1,21 @@
 package com.kodilla.exception.io;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile() {
+    public void readFile() throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("names.txt").getFile());
 
-        try (Stream<String> fileLines = new BufferedReader(new InputStreamReader(classLoader.getResourceAsStream("names.txt"), StandardCharsets.UTF_8)).lines()) {
+        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
             fileLines.forEach(System.out::println);
+        } catch (IOException e) {
+            throw new FileReaderException();
         } finally {
             System.out.println("I am gonna be here... always!");
         }
