8e86100adaa9a9f3e7b918b727be7838c1fa0236
Adrian Tretyn <adriantretyn@gmail.com>
2024-01-22 09:06:16 +0100

    task modulo 12.1

---
+++ kodilla-good-patterns/src/main/java/AdditionalTask.java
@@ -0,0 +1,17 @@
+public class AdditionalTask {
+    public static void main(String[] args) {
+        int n = 10;
+        long resolut = calculateFactoria(n);
+        System.out.println(resolut);
+    }
+
+    public static long calculateFactoria(int n){
+
+        if(n == 0|| n== 1){
+            return 1;
+        }
+        else{
+        return n * calculateFactoria(n - 1);
+    }
+
+}}
